173f0df90cdcbb3b39498572be60771df876fd63
Amey <ameyparab2007@gmail.com>
2017-05-05 12:30:27 -0700

    Removed unwanted code

--- angular-src/src/app/components/home/home.component.ts
+++ angular-src/src/app/components/home/home.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
-import { AuthService } from '../../services/auth.service';
 
 @Component({
   selector: 'app-home',
@@ -9,10 +8,7 @@ import { AuthService } from '../../services/auth.service';
 })
 export class HomeComponent implements OnInit {
 
-  constructor(private router: Router, private authService: AuthService) { 
-  	if(this.authService.loggedIn()){
-  		this.router.navigate(['/dashboard']);
-  	}
+  constructor() { 
   }
 
   ngOnInit() {
